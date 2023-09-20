bddd8a4a6075bba939fe0f492647b503e0cc6dbb
Victor Augusto <victor.liger0800@gmail.com>
2023-09-20 16:28:25 -0300

    Update 0.1 Criação do portfólio

---
+++ Controllers/PortController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Port.Controllers
+{
+    public class PortController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
